963d8e8628bbd7d0a49693c5a8f8e53ec807f470
Guillermo Garcia Perez <ggarciaperez@danielcastelao.org>
2020-11-25 12:48:16 +0100

    Migrated to ViewModel

--- app/src/main/java/com/dam2/simon/MainActivity.kt
+++ app/src/main/java/com/dam2/simon/MainActivity.kt
@@ -3,76 +3,113 @@ package com.dam2.simon
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
-import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
+
+//Un used since change to viewModel
 //    val text= "Hello Toast"
 //    val duration = Toast.LENGTH_SHORT
-    private var game = mutableListOf<Int>()
+//    private var game = mutableListOf<Int>()
 //    private var player = mutableListOf<Int>()
 
 
     @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
-        var player = mutableListOf<Int>()
+//Un used since change to viewModel
+//        var player = mutableListOf<Int>()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Adding viewmodel
         val simonModel by viewModels<SimonViewModel>()
+//Un used since change to viewModel
 //        val toast=Toast.makeText(applicationContext,text,duration)
 //        toast.show()
+
+        //Addig variables to game control buttons and textview
         val startButton: Button = findViewById(R.id.startButton)
         val resetButton: Button = findViewById(R.id.resetButton)
         val checkButton: Button = findViewById(R.id.checkButton)
         val outputText: TextView = findViewById(R.id.infoText)
 
+        //Adding variables for the user interaction
         val greenB:Button=findViewById(R.id.greenButton)
         val redB:Button=findViewById(R.id.redButton)
         val yellowB:Button=findViewById(R.id.yellowButton)
         val blueB:Button=findViewById(R.id.blueButton)
 
         simonModel.systemMoves.observe(this,
-            Observer{ newPlay -> outputText.text=systemMoves().toString()})
+            Observer{ newSystemMove -> outputText.text=newSystemMove.toString()
+            Log.d("FuncionSystem",newSystemMove.size.toString())
+            })
 
+        simonModel.playerMoves.observe(this, Observer {
+                newPlayerMove -> outputText.text=newPlayerMove.toString()
+        })
+
+        //adjusted buttons to work with the viewModel variables.
         greenB.setOnClickListener {
-            addPlayerMove(player,1) }
+            simonModel.addPlayerMove(simonModel.playerMoves,1) }
         redB.setOnClickListener {
-            addPlayerMove(player,2) }
+            simonModel.addPlayerMove(simonModel.playerMoves,2) }
         yellowB.setOnClickListener {
-            addPlayerMove(player,3) }
+            simonModel.addPlayerMove(simonModel.playerMoves,3) }
         blueB.setOnClickListener {
-            addPlayerMove(player,4) }
+            simonModel.addPlayerMove(simonModel.playerMoves,4) }
 
+        //calls viewModel Method to generate the system moves
         startButton.setOnClickListener {
             simonModel.systemPlays()
+            //Un used since change to viewModel
             //systemMoves();
             //outputText.text= game.toString()+"\n Do ur moves and press Check."
         }
+
+        //calls viewModel Method to reset the game variables
         resetButton.setOnClickListener {
-            game.clear()
-            player.clear()
+            simonModel.fullReset()
             outputText.text="Reinicio"
+            //Un used since change to viewModel
+            //game.clear()
+            //player.clear()
         }
 
+        // calls viewModel Method to check if the play is correct
         checkButton.setOnClickListener {
-            checkMoves(player,game)
-            player.clear()
+            outputText.text="Checking player moves\n"
+            simonModel.checkMoves(simonModel.systemMoves,simonModel.playerMoves)
+            //Un used since change to viewModel
+            //checkMoves(player,game)
+            //player.clear()
         }
 
 
     }
+
+    /* Obsolete methods since moved to viewModel
 
     private fun systemMoves(){
         val pos : Int
         val randInt = Random.nextInt(4)+1
         pos=game.size
         game.add(pos,randInt)
+    }
+
+    private fun addPlayerMove(player: MutableList<Int>, number: Int ){
+    when (number) {
+    1 -> player.add(1)
+    2 -> player.add(2)
+    3 -> player.add(3)
+    else -> player.add(4)
+    }
+    val outputText: TextView = findViewById(R.id.infoText)
+    outputText.text=player.toString()
     }
 
     private fun checkMoves(player: MutableList<Int>, game: MutableList<Int>){
@@ -85,33 +122,7 @@ class MainActivity : AppCompatActivity() {
             outputText.text= outputText.text.toString()+"You made a mistake, hit reset to start again."
         }
     }
+    */
 
-    /* NOT WORKING ATM private fun systemPlays(){
-        val greenB:Button=findViewById(R.id.greenButton)
-        val redB:Button=findViewById(R.id.redButton)
-        val yellowB:Button=findViewById(R.id.yellowButton)
-        val blueB:Button=findViewById(R.id.blueButton)
-        //val combS: TextView = findViewById(R.id.infoText)
-        for (c in (this.game)){
-            //combS.text=combS.text.toString()+c.toString()+"\n"
-            when (c){
-                1 -> greenB.isActivated
-                2 -> redB.shadowColor
-                3 -> yellowB.isPressed
-                4 -> blueB.highlightColor
-            }
-        }
-    }*/
-
-    private fun addPlayerMove(player: MutableList<Int>, number: Int ){
-        when (number) {
-            1 -> player.add(1)
-            2 -> player.add(2)
-            3 -> player.add(3)
-            else -> player.add(4)
-        }
-        val outputText: TextView = findViewById(R.id.infoText)
-        outputText.text=player.toString()
-    }
 
 }
--- app/src/main/java/com/dam2/simon/SimonViewModel.kt
+++ app/src/main/java/com/dam2/simon/SimonViewModel.kt
@@ -1,5 +1,7 @@
 package com.dam2.simon
 
+import android.util.Log
+import android.widget.TextView
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlin.random.Random
@@ -10,19 +12,56 @@ class SimonViewModel: ViewModel() {
     val systemMoves = MutableLiveData<MutableList<Int>>()
     val playerMoves = MutableLiveData<MutableList<Int>>()
 
+    //Initialice both variables
     init {
-        systemMoves.value!!.clear()
-        playerMoves.value!!.clear()
+        systemMoves.value= mutableListOf()
+        playerMoves.value= mutableListOf()
     }
 
+    //Method to force notify the observer about a change on a mutablelist
+    fun <T> MutableLiveData <T>.notifiObserver(){
+        this.value=this.value
+    }
+
+    //Method that adds a random value to the system variable
     fun systemPlays(){
         val pos : Int
         val randInt = Random.nextInt(4)+1
         pos= systemMoves.value!!.size
         this.systemMoves.value!!.add(pos,randInt)
+        //forces the variable update calling the observer
+        systemMoves.notifiObserver()
+        Log.d("FuncionSystem",systemMoves.value.toString())
     }
 
+    //Method to restart variables
+    fun fullReset(){
+        systemMoves.value!!.clear()
+        playerMoves.value!!.clear()
+    }
 
+    //Method to only restar player variable
+    fun playerReset(){
+        playerMoves.value!!.clear()
+    }
 
+    //Method to fill the player variable depending on button pressed
+    fun addPlayerMove(playerMoves: MutableLiveData<MutableList<Int>>, number: Int ){
+        when (number) {
+            1 -> playerMoves.value!!.add(1)
+            2 -> playerMoves.value!!.add(2)
+            3 -> playerMoves.value!!.add(3)
+            else -> playerMoves.value!!.add(4)
+        }
+        //forces the variable update calling observer
+        playerMoves.notifiObserver()
+    }
+
+    //Method to check if both variables are the same
+    fun checkMoves(system: MutableLiveData<MutableList<Int>>, player: MutableLiveData<MutableList<Int>>): Boolean{
+        return system==player
+    }
 
 }
+
+
